82c03b52e959efc620811486e8a9dcf9b7367d2c
Belle P <bellepx3@gmail.com>
2019-09-03 14:16:46 +0000

    Automatically backed up by Learn

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,16 @@
 class Manager
+attr_reader :name, :department, :age
+@@all = []
 
+def initialize(name, department, age)
+  @name = name
+  @department = department
+  @age = age
+  = manager_name
+  @@all << self
+end
+
+def self.all
+  @@all
+end
 end
