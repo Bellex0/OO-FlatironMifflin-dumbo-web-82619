489c4c276f7db8f0569cadaa322bd390af9dcbf1
Belle P <bellepx3@gmail.com>
2019-09-03 14:51:19 +0000

    Automatically backed up by Learn

--- run.rb
+++ run.rb
@@ -6,6 +6,7 @@ require 'pry'
 #Test your code here
 jason = Employee.new("Jason", 50000, "Belle")
 ben = Employee.new("Jason", 80000, "Belle")
+fiona = Employee.new("Fiona", 60000, "Belle")
 
 belle = Manager.new("Belle", "sales", 27)
 mia = Manager.new("Mia", "IT", 31)
