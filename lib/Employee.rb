d08419cbe13a501856d15df46118e01058388d8d
Belle P <bellepx3@gmail.com>
2019-09-03 14:15:15 +0000

    Automatically backed up by Learn

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,16 @@
 class Employee
+attr_reader :name, :salary, :manager_name
+@@all = []
 
+def initialize(name, salary, manager_name)
+  @name = name
+  @salary = salary
+  @manager_name = manager_name
+  @@all << self
 end
+
+def self.all
+  @@all
+end
+
+
